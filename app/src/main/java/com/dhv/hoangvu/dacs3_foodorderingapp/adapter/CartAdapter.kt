5c5d285b93a3501fa5bd2a5aec7494beea6db667
Đỗ Hữu Nhân <137048643+Nhandeptrai-123@users.noreply.github.com>
2024-05-07 20:10:34 +0700

    Completed Gio Hang

--- app/src/main/java/com/dhv/hoangvu/dacs3_foodorderingapp/adapter/CartAdapter.kt
+++ app/src/main/java/com/dhv/hoangvu/dacs3_foodorderingapp/adapter/CartAdapter.kt
@@ -1,18 +1,57 @@
 package com.dhv.hoangvu.dacs3_foodorderingapp.adapter
 
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.nfc.Tag
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.dhv.hoangvu.dacs3_foodorderingapp.databinding.CartItemBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlin.coroutines.coroutineContext
 
 class CartAdapter(
-    private val CartItems: MutableList<String>,
-    private val CartItemPrice: MutableList<String>,
-    private val CartImage: MutableList<Int>
+    private val context: Context,
+    private val cartItems: MutableList<String>,
+    private val cartItemPrices: MutableList<String>,
+    private val cartDescription:MutableList<String>,
+    private val cartImages: MutableList<String>,
+    private val cartQuantity: MutableList<Int>,
+    private val cartIngredient: MutableList<String>,
 ) : RecyclerView.Adapter<CartAdapter.CartViewHolder>() {
 
+    //khởi tạo firebase
+    private val auth = FirebaseAuth.getInstance()
 
-    private val itemQuantities = IntArray(CartItems.size) { 1 }
+    init {
+        val database = FirebaseDatabase.getInstance()
+        val useId = auth.currentUser?.uid?:""
+        val cartItemNumber = cartItems.size
+
+        itemQuantities = IntArray(cartItemNumber){1}
+        cartItemReference = database.reference.child("user").child(useId).child("GioHang")
+
+
+    }
+    companion object{
+        private var  itemQuantities: IntArray = intArrayOf()
+        private lateinit var cartItemReference: DatabaseReference
+    }
+
+//    private val itemQuantities = IntArray(cartItems.size) { 1 } --> DElete
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CartViewHolder {
 
@@ -24,24 +63,27 @@ class CartAdapter(
         holder.bind(position)
     }
 
-    override fun getItemCount(): Int {
-        return CartItems.size
-    }
+    override fun getItemCount(): Int = cartItems.size
 
     inner class CartViewHolder(private val binding: CartItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(position: Int) {
             binding.apply {
                 val quantity = itemQuantities[position]
-                cartFoodName.text = CartItems[position]
-                cartItemPrice.text = CartItemPrice[position]
-                cartImage.setImageResource(CartImage[position])
-                cartItemQuantity.text = quantity.toString()
+                cartFoodName.text = cartItems[position]
+                cartItemPrice.text = cartItemPrices[position]
 
+                //   sữ dụng Glide để load ảnh
+                val uriString = cartImages[position]
+                val uri = Uri.parse(uriString)
+                Glide.with(context).load(uri).into(cartImage)
+
+
+
+                cartItemQuantity.text = quantity.toString()
                 minusButton.setOnClickListener {
                     decreaseQuantity(position)
                 }
-
                 plusButton.setOnClickListener {
                     increaseQuantity(position)
                 }
@@ -71,13 +113,57 @@ class CartAdapter(
         }
 
         private fun deleteItem(position: Int) {
-            CartItems.removeAt(position)
-            CartItemPrice.removeAt(position)
-            CartImage.removeAt(position)
-            notifyItemRemoved(position)
-            notifyItemRangeChanged(position, CartItems.size)
+           val positionRetrieve = position
+            getUniqueKeyAtPosition(positionRetrieve){uniquekey ->
+                if (uniquekey != null){
+                    removeItem(position,uniquekey)
+                }
+            }
         }
 
 
     }
+
+    private fun removeItem(position: Int, uniquekey: String) {
+        if(uniquekey != null){
+            cartItemReference.child(uniquekey).removeValue().addOnCompleteListener{
+                cartItems.removeAt(position)
+                cartImages.removeAt(position)
+                cartDescription.removeAt(position)
+                cartQuantity.removeAt(position)
+                cartItemPrices.removeAt(position)
+                cartIngredient.removeAt(position)
+
+                Toast.makeText(context, "Xoá thành công", Toast.LENGTH_SHORT).show()
+                //uppdate lại số lượng
+                itemQuantities = itemQuantities.filterIndexed { index, i -> index != position }.toIntArray()
+                notifyItemRemoved(position)
+                notifyItemRangeChanged(position,cartItems.size)
+            }.addOnFailureListener {
+                Toast.makeText(context, "Xoá thất bại", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    private fun getUniqueKeyAtPosition(positionRetrieve: Int, onComplete: (String) -> Unit) {
+        cartItemReference.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                var uniquekey: String? = null
+                // vòng lặp qua các nhánh con snapshot
+                snapshot.children.forEachIndexed { index, dataSnapshot ->
+                    if (index == positionRetrieve) {
+                        uniquekey = dataSnapshot.key
+                        return@forEachIndexed
+                    }
+                }
+                onComplete(uniquekey ?: "") // Gọi onComplete ở đây sau khi uniquekey đã được xác định
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Xử lý khi có lỗi
+            }
+        })
+    }
+
 }
---
+++ app/src/main/java/com/dhv/hoangvu/dacs3_foodorderingapp/model/GioHangItem.kt
@@ -0,0 +1,10 @@
+package com.dhv.hoangvu.dacs3_foodorderingapp.model
+
+data class GioHangItem(
+    var foodName: String? = null,
+    var foodPrice   : String? = null,
+    var foodDescription: String? = null,
+    var foodImage: String? = null,
+    var foodQuantity: Int? = null,
+    var foodIngredient: String? = null
+)
