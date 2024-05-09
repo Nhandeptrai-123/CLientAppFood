1b51ab3ff18d5dc403e3530f067ad7d48a1e664c
xemdsmonan <huyln.22it@vku.udn.vn>
2024-05-09 13:53:43 +0700

    editprofile

---
+++ app/src/main/java/com/dhv/hoangvu/dacs3_foodorderingapp/model/UserModel.kt
@@ -0,0 +1,10 @@
+package com.dhv.hoangvu.dacs3_foodorderingapp.model
+
+data class UserModel(
+
+    val name:String?=null,
+    val email:String?=null,
+    val password:String?=null,
+    val phone:String?=null,
+    val address:String?=null,
+)
